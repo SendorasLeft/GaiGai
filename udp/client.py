9505f3f4d36655e00e3f0bc24aa119b580e3909a
Whitebells <sky.lasts.mirror@gmail.com>
2019-12-20 18:27:31 +0800

    misc changes

--- udp/client.py
+++ udp/client.py
@@ -17,7 +17,7 @@ player=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, output=True, frames
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
 client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-client.bind(("127.0.0.1", 37020))
+client.bind(("", 37020))
 print ("Your IP address is: ", socket.gethostbyname(socket.gethostname()))
 client.setblocking(0)
 
@@ -25,11 +25,10 @@ client.setblocking(0)
 
 
 while True:            #Used to continuously stream audio
-    print("hello")
     try:
         ready = select.select([client], [], [], timeout) # check if any data in socket
+        print(ready[0])
         if (ready[0]):
-            print("received")
             rcvdata, addr = client.recvfrom(256 * 2)
             player.write(rcvdata)
     except socket.timeout:
