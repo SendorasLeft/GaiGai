e2213bd6c605e3624ec4809b946356ccc880e32b
yjskrs <e0323600@u.nus.edu>
2019-12-22 15:04:13 +0800

    adjust volume parameters for rotary encoder

--- knob/rotary_encoder.py
+++ knob/rotary_encoder.py
@@ -1,3 +1,9 @@
+# Original code found at:
+# https://github.com/modmypi/Rotary-Encoder/
+# Install command for alsaaudio:
+# sudo -H pip3 install pyalsaaudio
+
+
 from RPi import GPIO
 from time import sleep
 import alsaaudio
@@ -9,28 +15,42 @@ def setup(clk, dt):
     GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
+def getVolumeChange(vol):
+    if vol >= 94:
+        return 1
+    elif vol >= 90:
+        return 2
+    elif vol >= 80:
+        return 3
+    elif vol >= 70:
+        return 4
+    elif vol >= 55:
+        return 5
+    else:
+        return 7
+
 def increaseVolume(mixer):
     vol = mixer.getvolume()[0]
-    if vol >= 95: # can't exceed 95 because it will be overly-loud
+    if vol >= 97: # can't exceed 96 because it will be overly-loud
         return
-    if vol + 5 > 95:
-        mixer.setvolume(95)
-        print("volume set to 95")
+    if vol + getVolumeChange(vol) > 97:
+        mixer.setvolume(97)
+        print("volume set to 97")
         return
-    mixer.setvolume(vol + 5)
-    print("volume set to", vol + 5)
+    mixer.setvolume(vol + getVolumeChange(vol))
+    print("volume set to", mixer.getvolume()[0])
 
 
 def decreaseVolume(mixer):
     vol = mixer.getvolume()[0]
-    if vol <= 0:
+    if vol <= 25: # can't exceed 25 because it will be too soft
         return
-    if vol - 5 < 0:
-        mixer.setvolume(0)
-        print("volume set to 0")
+    if vol - getVolumeChange(vol) < 25:
+        mixer.setvolume(25)
+        print("volume set to 25")
         return
-    mixer.setvolume(vol - 5)
-    print("volume set to", vol - 5)
+    mixer.setvolume(vol - getVolumeChange(vol))
+    print("volume set to", mixer.getvolume()[0])
 
 
 def controlVolume(clkState, dtState, clkLastState, mixer):
