64a70dadc3a26253c92d183523a54c3f19d322e0
Whitebells <sky.lasts.mirror@gmail.com>
2019-12-26 01:52:58 +0800

    Added mutex locks to mic and speaker read/write streams

--- central_server_udp/radio.py
+++ central_server_udp/radio.py
@@ -3,7 +3,7 @@ import numpy as np
 import socket
 import select
 import threading
-from threading import Thread
+from threading import Thread, Lock
 import struct
 import signal
 import sys
@@ -37,21 +37,33 @@ not_shutdown = True
 # initializes pyaudio reference, stream from microphone, player stream to speaker
 def IO_setup(rate, frames_per_buffer, channels=1, type_format=pyaudio.paInt16):
     p = pyaudio.PyAudio()
-    stream = p.open(format=type_format,rate=rate,channels=channels, input_device_index=2, input=True, frames_per_buffer=frames_per_buffer)
-    player = p.open(format=type_format,rate=rate,channels=channels, output_device_index=1, output=True, frames_per_buffer=frames_per_buffer)
-    return p, stream, player
+
+    stream = p.open(format=type_format
+                    ,rate=rate,channels=channels
+                    ,input_device_index=2
+                    ,input=True
+                    ,frames_per_buffer=frames_per_buffer)
+    player = p.open(format=type_format
+                    ,rate=rate
+                    ,channels=channels
+                    ,output_device_index=1
+                    ,output=True
+                    ,frames_per_buffer=frames_per_buffer)
+
+    stream_lock, player_lock = Lock(), Lock()
+    return p, stream, player, stream_lock, player_lock
 
 # initializes single multicast server port for streaming all mic sounds
 def server_multicast_setup(ttl, timeout):
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     server.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-    server.settimeout(0.1)
+    server.settimeout(timeout)
     return server
 
 def channel_preference_setup(ttl, timeout):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-    sock.settimeout(0.1)
+    sock.settimeout(timeout)
     return sock
 
 # given a multicast IP and a list of subscription ports,
@@ -68,16 +80,23 @@ def subscription_multicast_setup(multicast_group, port):
     return client
 
 # server thread that continually publishes mic audio using the server socket
-def server_thread(server_socket, stream, chunk_size, server_multicast_group):
+def server_thread(server_socket, stream, stream_lock, chunk_size, server_multicast_group):
     global not_shutdown
     while not_shutdown: # need to set flag for poweron/poweroff
-        data = np.fromstring(stream.read(chunk_size ,exception_on_overflow = False), dtype=np.int16)
+        data_string = read_from_stream(stream, stream_lock, chunk_size)
+        #data = np.fromstring(data_string, dtype=np.int16)
         #print(data)
-        server_socket.sendto(data.tostring(), server_multicast_group)
+        server_socket.sendto(data_string, server_multicast_group)
+
+def read_from_stream(stream, lock, chunk_size):
+    lock.acquire()
+    data = stream.read(chunk_size, exception_on_overflow=False)
+    lock.release()
+    return data
 
 # receiver thread that averages signal from all the subscribed sockets
 # TODO: set mode so that user can toggle between listening to all ports or to a specific port
-def receiver_thread(receiver_socket, player, timeout, chunk_size, rcv_multiplier):
+def receiver_thread(receiver_socket, player, player_lock, timeout, chunk_size, rcv_multiplier):
     global not_shutdown
     while not_shutdown: # need to set flag for poweron/off
         ready, _, _ = select.select([receiver_socket], [], [], timeout) # check if any data present in subscribed sockets
@@ -85,9 +104,14 @@ def receiver_thread(receiver_socket, player, timeout, chunk_size, rcv_multiplier
             if (receiver_socket in ready): # if socket has data
                 rcvdata, addr = receiver_socket.recvfrom(chunk_size * rcv_multiplier)
                 #print(rcvdata)
-                player.write(rcvdata)
+                play_sound(player=player, lock=player_lock, data=rcvdata)
         except socket.timeout:
             pass
+
+def play_sound(player, lock, data):
+    lock.acquire()
+    player.write(data)
+    lock.release()
 
 def channel_preference_thread(channel_socket, channel_multicast_group):
     global channel_preference, not_shutdown
@@ -98,16 +122,16 @@ def channel_preference_thread(channel_socket, channel_multicast_group):
 
 # driver function
 def main():
-    p, stream, player = IO_setup(rate=RATE, frames_per_buffer=CHUNK)
+    p, stream, player, stream_lock, player_lock = IO_setup(rate=RATE, frames_per_buffer=CHUNK)
     server_socket = server_multicast_setup(ttl=TTL, timeout=TIMEOUT)
     receiver_socket = subscription_multicast_setup(multicast_group=MULTICAST_IP, port=RECEIVER_PORT)
     channel_socket = channel_preference_setup(ttl=TTL, timeout=TIMEOUT)
 
     sending_thread = Thread(target=server_thread
-                                ,args=(server_socket, stream, CHUNK, server_multicast_group,))
+                                ,args=(server_socket, stream, stream_lock, CHUNK, server_multicast_group,))
 
     receiving_thread = Thread(target=receiver_thread
-                                ,args=(receiver_socket, player, TIMEOUT, CHUNK, RCV_MULTIPLIER,))
+                                ,args=(receiver_socket, player, player_lock, TIMEOUT, CHUNK, RCV_MULTIPLIER,))
 
     channelpref_thread = Thread(target=channel_preference_thread
                                     ,args=(channel_socket, channel_multicast_group,))
