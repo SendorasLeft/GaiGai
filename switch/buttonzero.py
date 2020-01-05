64b6b188cf35abec8b540628590b4ef066b5ed36
NinoMii <e0323600@u.nus.edu>
2020-01-05 16:10:48 +0800

    fix syntac

--- switch/buttonzero.py
+++ switch/buttonzero.py
@@ -3,10 +3,10 @@ from lcd_start_stop import display
 
 button = Button(26) # 19
 
-def switchedOn:
+def switchedOn():
     display(1)
 
-def switchedOff:
+def switchedOff():
     display(0)
 
 button.when_pressed = switchedOn
